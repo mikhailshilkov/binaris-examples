17c78e2fb2bbe3b077c9835b223a3165a899a588
Mikhail Shilkov <mikhail.shilkov@prodrive-technologies.com>
2018-12-22 09:53:21 +0100

    Adjust based on the tutorial

--- todo-backend-node/TodoBackend/function.js
+++ todo-backend-node/TodoBackend/function.js
@@ -1,19 +1,19 @@
 const rp = require('request-promise-native');
 
 const execute = async (command, field, value) => {
-  const key = 'TODOApp';
+  const request = {
+    command: command,
+    key: 'TodoBackend',
+    field: field,
+    value: JSON.stringify(value)
+  };
   const options = {
     url: `https://run-sandbox.binaris.com/v2/run/${process.env.BINARIS_ACCOUNT_NUMBER}/redis_todobackend`,
     headers: {
       'X-Binaris-Api-Key': `${process.env.BINARIS_API_KEY}`,
       'Content-Type': 'application/json',
     },
-    body: {
-      command: command,
-      key: key,
-      field: field,
-      value: JSON.stringify(value)
-    },
+    body: request,
     json: true
   };
   const response = await rp.post(options);
@@ -22,49 +22,8 @@ const execute = async (command, field, value) => {
     : undefined;
 }
 
-const handler = async (body, context) => {
-  let id = context.request.path.substring(1);
-
-  switch (context.request.method) {
-    case 'GET': {
-      if (id) {     
-        return await execute('hget', id);
-      } else {
-        const items = await execute('hvals');
-        return items.sort(i => i.order);
-      }
-    }
-
-    case 'POST': {
-      const todo = { 
-        completed: false,
-        id: Math.random().toString(36).substring(2),
-        ...body 
-      };
-      todo.url = `https://run-sandbox.binaris.com/v2/run/${process.env.BINARIS_ACCOUNT_NUMBER}/${process.env.BN_FUNCTION}/${todo.id}`;
-      return await execute('hset', todo.id, todo);
-    }
- 
-    case 'PATCH': {
-      const existing = await execute('hget', id);
-
-      const todo = { ...existing, ...body };
-      return await execute('hset', todo.id, todo);
-    }
-
-    case 'DELETE': {
-      if (id) {
-        await execute('hdel', id);
-      } else {
-        await execute('del');
-      }
-      return;
-    }
-  }
-};
-
-const cors = f => async (body, context) => {
-  const response = await f(body, context);
+const cors = handler => async (body, context) => {
+  const response = await handler(body, context);
 
   return new context.HTTPResponse({
     statusCode: 200,
@@ -75,6 +34,49 @@ const cors = f => async (body, context) => {
     },
     body: JSON.stringify(response)
   });
+};
+
+const handler = async (body, context) => {
+  let id = context.request.path.substring(1);
+
+  switch (context.request.method) {
+
+    case 'GET': {
+      if (id) {     
+        return await execute('hget', id);
+      } else {
+        const items = await execute('hvals');
+        return items.sort(i => i.order);
+      }
+    }
+
+    case 'POST': {
+      const generatedId = Math.random().toString(36).substring(2);
+      const todo = { 
+        completed: false,
+        id: generatedId,
+        url: `https://run-sandbox.binaris.com/v2/run/${process.env.BINARIS_ACCOUNT_NUMBER}/${process.env.BN_FUNCTION}/${generatedId}`,
+        ...body 
+      };
+      return await execute('hset', generatedId, todo);
+    }
+
+    case 'DELETE': {
+      if (id) {
+        await execute('hdel', id);
+      } else {
+        await execute('del');
+      }
+      return;
+    }
+
+    case 'PATCH': {
+      const existing = await execute('hget', id);
+
+      const todo = { ...existing, ...body };
+      return await execute('hset', todo.id, todo);
+    }
+  }
 };
 
 exports.handler = cors(handler);
